dcea18236741a28683ee43dc6dcf3f945e31f377
kevinpho1997 <kevin.pho@griffithuni.edu.au>
2022-08-15 23:19:53 +1000

    WIP update() and checkInput()

--- start_menu/button.py
+++ start_menu/button.py
@@ -1,11 +1,21 @@
 class Button():
-    def __init__(self, img, coor, font, display_text, colour, hover_colour):
+    def __init__(self, img, coor, font, display_text, colour):
         self.img = img
         self.x_coor = coor[0]
         self.y_coor = coor[1]
         self.font = font
         self.colour = colour
-        self.hover_colour = hover_colour
+        # change colour on hover --> do it later
+        # self.hover_colour = hover_colour
         self.display_text = display_text
         # rendering text
         self.text = self.font.render(self.display_text, True, self.colour)
+        # if no image is found
+        if self.img is None:
+            self.img = self.display_text
+        
+        def update(self, screen):
+            pass
+
+        def checkInput(self, coordinate):
+            pass
