1cb95046377d0f67303438e80be3a65dabc9388b
paulsmythev <paul_smyth@easternvisions.com>
2022-09-22 14:08:23 +1000

    temporary classes and functions

--- top_score/file_handler.py
+++ top_score/file_handler.py
@@ -3,29 +3,26 @@ import json
 
 FILE_PATH = "top_score/top-scores.json"
 
-class FileHandler():
-    def __init__(self):
-        pass
+class FileHandler:
+    #readJson() loads the top scores saved in the "top_score/top-scores.json", if the file cant be found then a error is diplayed here and in the top_score_screen.py   
+    def read_json(self):
+        try:
+            file = open(FILE_PATH)
+            top_scores_array = json.load(file)
+            file.close()
+        except:
+            print("file_handler.py - Error loading JSON file")
 
-#readJson() loads the top scores saved in the "top_score/top-scores.json", if the file cant be found then a error is diplayed here and in the top_score_screen.py   
-def read_json():
-    try:
-        file = open(FILE_PATH)
-        top_scores_array = json.load(file)
-        file.close()
-    except:
-        print("file_handler.py - Error loading JSON file")
+        return top_scores_array
 
-    return top_scores_array
+    #writeJson(updatedScoreList) receives an array that then over-writes the data in "top_score/top-scores.json" then returns a Boolean depending on success 
+    def write_json(self, updated_score_list):
+        write_success = False
+        try:
+            with open(FILE_PATH, "w", encoding="utf-8") as f:
+                json.dump(updated_score_list, f, indent=1)
+                write_success = True
+        except:
+            print("Error writing to JSON")
 
-#writeJson(updatedScoreList) receives an array that then over-writes the data in "top_score/top-scores.json" then returns a Boolean depending on success 
-def write_json(updated_score_list):
-    write_success = False
-    try:
-        with open(FILE_PATH, "w", encoding="utf-8") as f:
-            json.dump(updated_score_list, f, indent=1)
-            write_success = True
-    except:
-        print("Error writing to JSON")
-
-    return write_success
+        return write_success
